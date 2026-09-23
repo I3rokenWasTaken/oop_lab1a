3b7bac165c132209571cb92768210f9161e841ba
Mohammad Tahsin Bhuyan <G00467010@atu.ie>
2026-09-23 17:50:00 +0100

    Added helper method in main and updated Readme file

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 Book Tracker application
+Added Book class
+Added helper method created book 1,2,3,4 book 1 had values where it can be loaned or not, has if else statements. book 2,3,4 was created with title, author and page count
--- src/main/java/ie/atu/oop/week1/main.java
+++ src/main/java/ie/atu/oop/week1/main.java
@@ -3,7 +3,8 @@ package ie.atu.oop.week1;
 import java.sql.SQLOutput;
 
 public class main {
-    public static void main(String[] args) {
+    public static void main(String[] args)
+    {
 
         Book firstbook = new Book();
         firstbook.title = "Dune";
@@ -15,5 +16,25 @@ public class main {
         firstbook.borrowBook();
         //after loan
         firstbook.displayDetails();
+
+        Book secondbook = createBook("Clean Code", "Dan Williams", 223);
+        Book thirdbook = createBook("Another Book Code", "John Murphy", 333);
+        Book fourthbook = createBook("Student Life", "Merry Shelley", 442);
+
+        System.out.println("\n");
+        secondbook.displayDetails();
+        System.out.println("\n");
+        thirdbook.displayDetails();
+        System.out.println("\n");
+        fourthbook.displayDetails();
+    }
+
+    private static Book createBook(String title, String author, int pageCount)
+    {
+        Book book = new Book();
+        book.title = title;
+        book.author = author;
+        book.pageCount = pageCount;
+        return book;
     }
 }
